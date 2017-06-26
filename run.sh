482f7d4a61dd443c32d8790daa0ddc761d269683
AlmogBaku <almog.baku@gmail.com>
2017-06-26 15:27:58 +0300

    avoid glide cache

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 
 info "Verify code formatting with gofmt"
 
-FILES=$(gofmt -l `find . -type f -name "*.go" | grep -v vendor`)
+FILES=$(gofmt -l `find . -type f -name "*.go" | grep -v vendor | grep -v .glide`)
 if [[ ! -z "$FILES" ]]; then
   echo The following files aren\'t formatted well: $'\n' $FILES
   exit 1
